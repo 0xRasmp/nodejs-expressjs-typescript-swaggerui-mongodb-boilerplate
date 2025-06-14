e14b581580f26e029e46a315bcb981f9743a352c
Syntaxloom <pkehinde27@gmail.com>
2025-06-14 13:35:59 +0100

    successfully added tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/public/**/*.{html,js,ts}",
+    "./dist/public/**/*.{html,js,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
